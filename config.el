2440b404e15f3d263a041beace98f13f36d38247
Anton Satin <astair36@gmail.com>
2020-03-16 09:49:31 +0200

    Added copy file to clipboard shortcut

--- config.el
+++ config.el
@@ -32,5 +32,20 @@
               (typescript-mode)
               )))
 
+(defun copy-filename-to-clipboard ()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (with-temp-buffer
+        (insert filename)
+        (clipboard-kill-region (point-min) (point-max)))
+      (message filename))))
 
-(toggle-frame-fullscreen)
+(map! :leader
+      "f p"
+      'copy-filename-to-clipboard)
+
+;; (toggle-frame-fullscreen)
